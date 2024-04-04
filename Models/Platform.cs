07f57eb93ce351ac56dc7eb991ab5c8f840dd6a1
GuilhermeUhrigshardt <guilhermeuh@gmail.com>
2024-04-04 08:26:25 -0300

    Platform model

--- Models/Platform.cs
+++ Models/Platform.cs
@@ -1,20 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace PlatformService.Models
+namespace CommandsService.Models
 {
     public class Platform
     {
-        [Key]
-        [Required]
         public int Id { get; set; }
-
-        [Required]
+        public int ExternalID { get; set; }
         public string? Name { get; set; }
-
-        [Required]
-        public string? Publisher { get; set; }
-
-        [Required]
-        public string? Cost { get; set; }
     }
 }
